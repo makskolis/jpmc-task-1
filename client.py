3d3dc8e81c4714fcdce5ff1acd9821e306afa499
Maksymilian Kolis <maksymiliankolis@gmail.com>
2022-12-16 19:33:15 +0000

    ✨ Implemented getRatio()
    Implemented the getRatio() function to now properly display the ratio of two stocks

--- client.py
+++ client.py
@@ -32,17 +32,22 @@ N = 500
 def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
     """ ------------- Update this function ------------- """
+    if (not quote):
+        return warn("getDataPoint: argument missing")
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price)/2
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    if (not price_a or price_a == 0) or (not price_b or price_b == 0):
+        warn("getRatio: invalid arguments: missing or 0")
+        return
+    return price_a/price_b
 
 
 # Main
@@ -52,8 +57,9 @@ if __name__ == "__main__":
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
+        prices = {}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+        print("Ratio %s" % getRatio(prices["ADC"], prices["DEF"]))
